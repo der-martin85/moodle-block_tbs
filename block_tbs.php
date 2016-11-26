f7a2795a72cd1bfe7f5695997e6b859b540a153e
fschuett <fschuett@gymnasium-himmelsthuer.de>
2016-11-26 16:09:28 +0100

    rename files according to new task

---
+++ block_tbs.php
@@ -0,0 +1,71 @@
+<?php
+
+// This file is part of the TBS block for Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+class block_tbs extends block_base {
+
+    function init() {
+        $this->title = get_string('blockname', 'block_tbs');
+        $this->content_type = BLOCK_TYPE_TEXT;
+    }
+
+    function has_config() {
+        return true;
+    }
+
+    function applicable_formats() {
+        return array('all' => true);
+    }
+
+    function get_content() {
+        global $CFG, $OUTPUT;
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $cfg_tbs = get_config('block/tbs');
+
+        $context = context_system::instance();
+
+        if (has_capability('block/tbs:viewtbs', $context) or has_capability('block/tbs:edittbs', $context) or has_capability('block/tbs:administertbs', $context)) {
+            if (isset($CFG->block_tbs_serverpath)) {
+                $serverpath = $CFG->block_tbs_serverpath;
+            } else {
+                $serverpath = $CFG->wwwroot.'/blocks/tbs/web';
+            }
+            $go = get_string('accesstbs', 'block_tbs');
+            $icon = '<img src="'.$OUTPUT->pix_url('web', 'block_tbs').'" height="16" width="16" alt="" />';
+            $target = '';
+            if ($cfg_tbs->newwindow) {
+                $target = ' target="_blank" ';
+            }
+            $this->content = new stdClass();
+            $this->content->text = '<a href="'.$serverpath.'/index.php" '.$target.'>'.$icon.' &nbsp;'.$go.'</a>';
+            $this->content->footer = '';
+            return $this->content;
+        }
+
+        return null;
+    }
+
+    function cron() {
+        global $CFG;
+        include($CFG->dirroot.'/blocks/tbs/import.php');
+
+        return true;
+    }
+}
